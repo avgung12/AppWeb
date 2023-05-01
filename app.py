2e2088a5094f3baafceec2ba989760e9db620f47
avgung12 <118052124+avgung12@users.noreply.github.com>
2023-05-01 18:36:31 +0000

    Implementing Pagination + Dynamic Filters
    Implement Pagination, though datatables and filtering through departments.
    
    -Removed department-filter function.
    -PopulateDepartmentFilter created to add information from
    
    department-filter
    
    DepartmentValues [] are added from table which are distinct.
    
     If the departmentValues does not include the information it gets appended.
    
    The department filter is then populated.
    
    - Implemented hidden rows, when each query is clicked all information is displayed. This includes profile pictures.

--- app.py
+++ app.py
@@ -30,19 +30,38 @@ def home():
 
 @app.route('/search_results', methods=['GET', 'POST'])
 def search_results():
+
     query = str(request.form.get('query')).strip()
+
     search_by = request.form.get('search_by')
+
+    if search_by is None:
+
+        search_by="AllFields"
+
+        query=""
+
     conn = connection()
+
     cursor = conn.cursor()
-    if search_by=="AllFields" and query != "*":
+
+    if query == "*" or len(query)==0:
+
+        mySQL=f"SELECT * from dbo.Persons"  
+
+    elif search_by=="AllFields":
+
         mySQL=f"SELECT * from dbo.Persons where firstname=\'{query}\' or lastname=\'{query}\' or phone=\'{query}\' or office=\'{query}\' or emailaddress=\'{query}\' or Departement=\'{query}\' or division=\'{query}\' or title=\'{query}\'"    
-    elif query == "*":
-        mySQL=f"SELECT * from dbo.Persons"    
+
     else:
+
         mySQL=f"SELECT * from dbo.Persons where {search_by}=\'{query}\'"
+
     cursor.execute(mySQL)
+
     results = cursor.fetchall()
-    return render_template('search_results.html', results=results)
+
+    return render_template('search_results.html', results=results,query=query,search_by=search_by)
 
 
 @app.route("/hello/")
