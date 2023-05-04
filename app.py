b9bebb46e5540568387dc332f016d0dceeda4eb1
avgung12 <118052124+avgung12@users.noreply.github.com>
2023-05-04 21:53:31 +0000

    Fixed Sorting and Department Filtering
    - Properly fixed indexing for Sorting and Department Filter.
    
    -Fixed an issue regarding not being able to filter departments due to & and &amp;.
    
    Pending Issues: When searching all fields, does not automatically sort by First Name.

--- app.py
+++ app.py
@@ -21,72 +21,23 @@ def connection():
 def home():
     return render_template("searchiframe.html")
 
-#@app.route('/search_results', methods=['GET', 'POST'])
-#def search_results():
-#    query = request.form.get('query')
-#    search_by = request.form.get('search_by')
-#    results = ["result 1", "result 2", "result 3"]
-#    return render_template('search_results.html', query=query, search_by=search_by,results=results)
-
 @app.route('/search_results', methods=['GET', 'POST'])
 def search_results():
-
     query = str(request.form.get('query')).strip()
-
     search_by = request.form.get('search_by')
-
     if search_by is None:
-
         search_by="AllFields"
-
         query=""
-
     conn = connection()
-
     cursor = conn.cursor()
-
     if query == "*" or len(query)==0:
-
         mySQL=f"SELECT * from dbo.Persons"  
-
     elif search_by=="AllFields":
-
         mySQL=f"SELECT * from dbo.Persons where firstname=\'{query}\' or lastname=\'{query}\' or phone=\'{query}\' or office=\'{query}\' or emailaddress=\'{query}\' or Departement=\'{query}\' or division=\'{query}\' or title=\'{query}\'" 
-           
-
     else:
-
         mySQL=f"SELECT * from dbo.Persons where {search_by}=\'{query}\'"
-
     cursor.execute(mySQL)
-
     results = cursor.fetchall()
-
     return render_template('search_results.html', results=results,query=query,search_by=search_by)
-
-
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello_there(name = None):
-    return render_template(
-        "hello_there.html",
-        name=name,
-        date=datetime.now()
-    )
-
-@app.route("/api/data")
-def get_data():
-    return app.send_static_file("data.json")
-
-# New functions
-@app.route("/about/")
-def about():
-    return render_template("about.html")
-
-@app.route("/contact/")
-def contact():
-    return render_template("contact.html")
-
-
 if __name__ == '__main__':
     app.run(debug=True)
