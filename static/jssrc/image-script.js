4297098f4d513def4b11b89711d0f4290bb52273
avgung12 <118052124+avgung12@users.noreply.github.com>
2023-05-25 20:18:54 +0000

    Reobfuscating files
    - Add separate variables to connect to URL. Create storageAccount variable and containerName.
    
    -Reobfuscate files: frame_o.js, image-script_o.js, results_o.js

--- static/jssrc/image-script.js
+++ static/jssrc/image-script.js
@@ -1,7 +1,9 @@
 (function() {
   var index = document.currentScript.getAttribute('data-index');
   var email = document.currentScript.getAttribute('data-email');
-  var img_url = 'https://testwebdbphotos.blob.core.windows.net/pics/' + email.split("@")[0] + '.jpg';
+  var storageAccount = "testwebdbphotos";
+  var containerName = "pics";
+  var img_url = 'https://'+ storageAccount + '.blob.core.windows.net/' + containerName + '/' + email.split("@")[0] + '.jpg';
   var connector = "?"; 
   var sas = "si=OneWeek&spr=https&sv=2022-11-02&sr=c&sig=69q43gwrC7dDO48JccVxeQrhqy%2Bd8%2FJlCNl6Ob2nMxI%3D";
   var img_url = img_url + connector + sas;
