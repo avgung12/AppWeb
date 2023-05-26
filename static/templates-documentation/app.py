85d66c314724fe67f647a6f1bf84718e378b484c
avgung12 <118052124+avgung12@users.noreply.github.com>
2023-05-26 23:15:43 +0000

    Create app.py
    Document App.py

---
+++ static/templates-documentation/app.py
@@ -0,0 +1,102 @@
+import re
+from datetime import datetime
+from flask import Flask,request  ##Imports Flask and request function to display HTML. 
+import requests
+from flask import render_template
+import os
+
+app = Flask(__name__, static_folder='static')
+
+import pyodbc                 #Imports pydobc to connect Flask to Microsoft Server. 
+def connection():
+    server = 'ericsqlserver.database.windows.net' ##Credentials to connect to Microsoft Server. 
+    database = 'ericsqldb'
+    username = 'devuser0'
+    password = 'Python2023'
+    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};' ##OBDC Driver to connect to SQL server, ODBC Driver may depend on other systems. 
+                      'SERVER=ericsqlserver.database.windows.net;'
+                      'DATABASE=ericsqldb;UID=devuser0;PWD=Python2023;')
+    return conn
+
+##import pypyodbc
+##def connection():
+  ##  myConString = os.environ['CUSTOMCONNSTR_DBConStr']
+   ## conn = pypyodbc.connect(myConString)
+    ##return conn
+
+@app.route("/")         ##Sets the home page route to "searchiframe_hidejs.html", when visiting the website. 
+def home():
+    return render_template("searchiframe_hidejs.html") ##"searchiframe_hidejs.html" hosts the iframe. 
+
+@app.route('/search_results', methods=['GET', 'POST'])  
+def search_results():
+    query = str(request.form.get('query')).strip()
+    search_by = request.form.get('search_by')
+    if search_by is None:
+        search_by="AllFields"
+        query=""
+    conn = connection()
+    cursor = conn.cursor()
+
+    #storageAccount=os.environ['env_storageAccount']
+    #containerName=os.environ['env_containerName']
+    #table=os.environ['env_myTable']
+    storageAccount="testwebdbphotos" #Parametrizing image URL Direcotry
+    containerName="pics" #Parameterizing image folder Directory
+    table="ITSTAFF" ##Table variable to parameterize table selection or declaration.
+    orderBy=f"order by Name ASC"
+    
+
+## Modify the code to implement security measures against SQL injections. via Parametrization
+    placeholders = [] ## placeholder are used to search queries. 
+    conditions = [] ## holds the amount of conditions. 
+    
+    if query == "*" or len(query) < 2 :   ##If the query length is less than 2, or an asterisk it will return empty. 
+        #mySQL=f"SELECT * from dbo.{table}" 
+        mySQL=f"SELECT * FROM dbo.{table} WHERE 1=2" 
+    elif search_by=="AllFields":   ##Else, if the query length > 3 and All Fields are searched, it will list multiple conditions it can be searched by. 
+        conditions = [              ##name, firstname, lastname, phone, office, emailaddress, department, division, title. 
+        "name LIKE ?",
+        "firstname LIKE ?",
+        "lastname LIKE ?",
+        "phone LIKE ?",
+        "office LIKE ?",
+        "emailaddress LIKE ?",
+        "Departement LIKE ?",
+        "division LIKE ?",
+        "title LIKE ?"
+        ]
+        placeholders = ['%' + query + '%'] * len(conditions) ##Placeholders in conjunction with conditions .
+                                                             ## Will query the amount of times, equal to the length of conditions, 
+                                                             ## Name LIKE %Bill%, firstname LIKE %BILL%, lastname LIKE %BILL%.......
+    else:
+       conditions.append(f"{search_by} LIKE ?") ##On the other hand, if its not search by All Fields, it will be based on "search_by" 
+       placeholders.append('%' + query + '%') ##The query will then be appended,  search by = Name, therefore f"{Name} Like ? '%' + Bill + '%'
+       
+    if conditions:
+        where_clause = " OR ".join(conditions) ##If there are existing conditions, then an "or" operator will be joined along the conditions    
+                                               ## Name LIKE %Bill% OR  firstname LIKE %BILL% OR lastname LIKE %BILL%.
+                                               
+        mySQL = f"SELECT * FROM dbo.{table} WHERE {where_clause}" ##mySQL combines where_clause with table
+                                                                  ## THIS CREATES A PARAMETERIZED query, this not pure query. 
+        
+    mySQL+= " " + orderBy      ##Concatenated with orderBy, will exucute search with conditions and orderBy string. 
+    cursor.execute(mySQL, placeholders) ##Example: "SELECT * FROM dbo.Persons WHERE Name like %Bill% or FirstName %Bill% order by Name ASC"
+    results = cursor.fetchall()
+    return render_template('search_results_hidejs.html', results=results,storageAccount=storageAccount,containerName=containerName)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#Deploy to Azure:
+#1. Comment out: 'import pyodbc'. Add: import pypyodbc & import os
+#2. pip freeze>requirements.txt(skip this step if already done)
+#3. Create configuration in “Settings|Configuration”:
+#   Name: DBConStr
+#   Value:DRIVER={ODBC Driver 17 for SQL Server};SERVER=tcp:ericsqlserver.database.windows.net;PORT=1433;DATABASE=ericsqldb;Authentication=ActiveDirectoryMSI
+#   Type: Custom
+#4. Replace with new DB connect function:
+#def connection():
+#    myConString = os.environ['CUSTOMCONNSTR_DBConStr']
+#    conn = pypyodbc.connect(myConString)
+#    return conn   
